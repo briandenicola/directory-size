42b361e1bed260d059b95628eaabaf58fade46a5
Brian Denicola <bjd145@yahoo.com>
2020-11-20 22:38:23 -0600

    Top-level programs feature of C#9

--- csharp9/Program.cs
+++ csharp9/Program.cs
@@ -1,34 +1,24 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.CommandLine;
 using System.CommandLine.Invocation;
-using System.Threading.Tasks;
+using DirectorySize;
 
-namespace DirectorySize
+if(args.Length == 0 ) 
+    args = new string[1] { "-h" };
+
+var rootCommand = new RootCommand
 {
-    class Program
-    {
-        static int Main(string[] args) 
-        {
-            if(args.Length == 0 ) 
-                args = new string[1] { "-h" };
+    new Option<DirectoryInfo>(new [] {"--path", "-p"},description: "The folder path to check size of"),
+    new Option<bool>(new [] {"--quiet", "-q"},description: "Quiet output"),
+    new Option<bool>("--show-errors", description: "An option whose argument is parsed as a FileInfo")
+};
 
-            var rootCommand = new RootCommand
-            {
-                new Option<DirectoryInfo>(new [] {"--path", "-p"},description: "The folder path to check size of"),
-                new Option<bool>(new [] {"--quiet", "-q"},description: "Quiet output"),
-                new Option<bool>("--show-errors", description: "An option whose argument is parsed as a FileInfo")
-            };
+rootCommand.Description = "A console app to show the size of all subfolders under itself";
+rootCommand.Handler = CommandHandler.Create<DirectoryInfo, bool, bool>( (path, quiet, showErrors) =>
+{   
+    var repo = new DirectoryRepository(path.FullName);
+    repo.Run();
+    repo.Print(showErrors, quiet);
+});
 
-            rootCommand.Description = "A console app to show the size of all subfolders under itself";
-            rootCommand.Handler = CommandHandler.Create<DirectoryInfo, bool, bool>( (path, quiet, showErrors) =>
-            {   
-                var repo = new DirectoryRepository(path.FullName);
-                repo.Run();
-                repo.Print(showErrors, quiet);
-            });
-
-            return rootCommand.InvokeAsync(args).Result;
-        }
-    }
-}
+return rootCommand.InvokeAsync(args).Result;
