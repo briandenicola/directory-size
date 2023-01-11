0b03dda7c9cf5e48f7752dd1defef12fd81a3faf
briandenicola <brian@briandenicola.com>
2023-01-11 15:35:48 -0600

    Basic dotnet7 example

---
+++ csharp11/Common/DirectoryOutput.cs
@@ -0,0 +1,43 @@
+namespace DirectorySize;
+public class DirectoryOutput
+{
+    const double MB = 1048576.0;
+    Table output = new Table();
+    TableColumn pathColumn= new TableColumn("Path");
+    TableColumn countColumn = new TableColumn("Files");
+    TableColumn sizeColumn = new TableColumn("Size (MB)");
+
+    public DirectoryOutput() 
+    {
+        output.Border(TableBorder.DoubleEdge);
+        output.Centered();
+        output.Width(System.Console.WindowWidth);
+
+        output.AddColumn(pathColumn);
+        output.AddColumn(countColumn);
+        output.AddColumn(sizeColumn);
+    }
+    
+    private static string ToNumberFormat(long val) => string.Format("{0:#,0}", val);
+    private static string ToMB(long val) => string.Format("{0:#,0.00}", (double) Math.Round((double) val / MB, 2));
+    
+    public void DisplayResults( ConcurrentDictionary<string,DirectoryStatistics> repo, long size, long count, long time, int errors) 
+    {   
+        System.Console.WriteLine();
+
+        foreach (var directory in repo.OrderByDescending( o => o.Value.DirectorySize))
+        {
+            output.AddRow(
+                new Text(directory.Value.Path),
+                new Text(ToNumberFormat(directory.Value.FileCount)),
+                new Text(ToMB(directory.Value.DirectorySize))
+            );
+        }
+
+        pathColumn.Footer(new Text("Totals:"));
+        countColumn.Footer(new Text(ToNumberFormat(count)));
+        sizeColumn.Footer(new Text(ToMB(size)));
+
+        AnsiConsole.Write(output);
+    }
+}
---
+++ csharp11/publish.bat
@@ -0,0 +1,5 @@
+@echo off
+
+dotnet publish -c Release -r osx.11.0-arm64 --self-contained --nologo -o publish/mac -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true
+dotnet publish -c Release -r linux-x64 --self-contained --nologo -o publish/linux -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true
+dotnet publish -c Release -r win10-x64 --self-contained --nologo -o publish/windows -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true
